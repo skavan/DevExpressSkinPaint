7fda27a92f8b68c68924a0721f3be6a98187a677
Suresh Kavan <suresh.kavan@gmail.com>
2016-02-12 23:26:31 -0500

    SkinPaint

---
+++ frmSkinTest/Form1.Designer.vb
@@ -0,0 +1,255 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits DevExpress.XtraEditors.XtraForm
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim ContextButton1 As DevExpress.Utils.ContextButton = New DevExpress.Utils.ContextButton()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Dim TileViewItemElement1 As DevExpress.XtraGrid.Views.Tile.TileViewItemElement = New DevExpress.XtraGrid.Views.Tile.TileViewItemElement()
+        Dim TileViewItemElement2 As DevExpress.XtraGrid.Views.Tile.TileViewItemElement = New DevExpress.XtraGrid.Views.Tile.TileViewItemElement()
+        Dim TileViewItemElement3 As DevExpress.XtraGrid.Views.Tile.TileViewItemElement = New DevExpress.XtraGrid.Views.Tile.TileViewItemElement()
+        Dim TileViewItemElement4 As DevExpress.XtraGrid.Views.Tile.TileViewItemElement = New DevExpress.XtraGrid.Views.Tile.TileViewItemElement()
+        Me.colID = New DevExpress.XtraGrid.Columns.TileViewColumn()
+        Me.colInfo = New DevExpress.XtraGrid.Columns.TileViewColumn()
+        Me.colCheck = New DevExpress.XtraGrid.Columns.TileViewColumn()
+        Me.colButton = New DevExpress.XtraGrid.Columns.TileViewColumn()
+        Me.RepositoryItemButtonEdit1 = New DevExpress.XtraEditors.Repository.RepositoryItemButtonEdit()
+        Me.FormAssistant1 = New DevExpress.XtraBars.FormAssistant()
+        Me.DefaultLookAndFeel1 = New DevExpress.LookAndFeel.DefaultLookAndFeel()
+        Me.PanelControl1 = New DevExpress.XtraEditors.PanelControl()
+        Me.GridControl1 = New DevExpress.XtraGrid.GridControl()
+        Me.TileView1 = New DevExpress.XtraGrid.Views.Tile.TileView()
+        Me.PanelControl2 = New DevExpress.XtraEditors.PanelControl()
+        Me.SimpleButton1 = New DevExpress.XtraEditors.SimpleButton()
+        Me.SimpleButton2 = New DevExpress.XtraEditors.SimpleButton()
+        Me.SimpleButton3 = New DevExpress.XtraEditors.SimpleButton()
+        Me.SimpleButton4 = New DevExpress.XtraEditors.SimpleButton()
+        Me.SimpleButton5 = New DevExpress.XtraEditors.SimpleButton()
+        Me.SimpleButton6 = New DevExpress.XtraEditors.SimpleButton()
+        CType(Me.RepositoryItemButtonEdit1,System.ComponentModel.ISupportInitialize).BeginInit
+        CType(Me.PanelControl1,System.ComponentModel.ISupportInitialize).BeginInit
+        Me.PanelControl1.SuspendLayout
+        CType(Me.GridControl1,System.ComponentModel.ISupportInitialize).BeginInit
+        CType(Me.TileView1,System.ComponentModel.ISupportInitialize).BeginInit
+        CType(Me.PanelControl2,System.ComponentModel.ISupportInitialize).BeginInit
+        Me.SuspendLayout
+        '
+        'colID
+        '
+        Me.colID.Caption = "ID"
+        Me.colID.FieldName = "ID"
+        Me.colID.Name = "colID"
+        Me.colID.Visible = true
+        Me.colID.VisibleIndex = 0
+        '
+        'colInfo
+        '
+        Me.colInfo.Caption = "Info"
+        Me.colInfo.FieldName = "Info"
+        Me.colInfo.Name = "colInfo"
+        Me.colInfo.Visible = true
+        Me.colInfo.VisibleIndex = 1
+        '
+        'colCheck
+        '
+        Me.colCheck.Caption = "Check"
+        Me.colCheck.FieldName = "Check"
+        Me.colCheck.Name = "colCheck"
+        Me.colCheck.Visible = true
+        Me.colCheck.VisibleIndex = 2
+        '
+        'colButton
+        '
+        Me.colButton.Caption = "MyButton"
+        Me.colButton.ColumnEdit = Me.RepositoryItemButtonEdit1
+        Me.colButton.FieldName = "colButton"
+        Me.colButton.Name = "colButton"
+        Me.colButton.UnboundType = DevExpress.Data.UnboundColumnType.[Object]
+        Me.colButton.Visible = true
+        Me.colButton.VisibleIndex = 3
+        '
+        'RepositoryItemButtonEdit1
+        '
+        Me.RepositoryItemButtonEdit1.AutoHeight = false
+        Me.RepositoryItemButtonEdit1.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() {New DevExpress.XtraEditors.Controls.EditorButton()})
+        Me.RepositoryItemButtonEdit1.Name = "RepositoryItemButtonEdit1"
+        '
+        'PanelControl1
+        '
+        Me.PanelControl1.Controls.Add(Me.GridControl1)
+        Me.PanelControl1.Controls.Add(Me.SimpleButton5)
+        Me.PanelControl1.Location = New System.Drawing.Point(112, 303)
+        Me.PanelControl1.Margin = New System.Windows.Forms.Padding(6)
+        Me.PanelControl1.Name = "PanelControl1"
+        Me.PanelControl1.Padding = New System.Windows.Forms.Padding(6)
+        Me.PanelControl1.Size = New System.Drawing.Size(624, 567)
+        Me.PanelControl1.TabIndex = 0
+        '
+        'GridControl1
+        '
+        Me.GridControl1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.GridControl1.Location = New System.Drawing.Point(9, 76)
+        Me.GridControl1.MainView = Me.TileView1
+        Me.GridControl1.Name = "GridControl1"
+        Me.GridControl1.Padding = New System.Windows.Forms.Padding(2)
+        Me.GridControl1.RepositoryItems.AddRange(New DevExpress.XtraEditors.Repository.RepositoryItem() {Me.RepositoryItemButtonEdit1})
+        Me.GridControl1.Size = New System.Drawing.Size(606, 482)
+        Me.GridControl1.TabIndex = 1
+        Me.GridControl1.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.TileView1})
+        '
+        'TileView1
+        '
+        Me.TileView1.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() {Me.colID, Me.colInfo, Me.colCheck, Me.colButton})
+        ContextButton1.Alignment = DevExpress.Utils.ContextItemAlignment.CenterFar
+        ContextButton1.Caption = "Button"
+        ContextButton1.Glyph = CType(resources.GetObject("ContextButton1.Glyph"),System.Drawing.Image)
+        ContextButton1.Height = 32
+        ContextButton1.Id = New System.Guid("b5701d27-0b29-4276-a571-274cf2345073")
+        ContextButton1.MaxHeight = 32
+        ContextButton1.MaxWidth = 32
+        ContextButton1.Name = "ContextButton"
+        ContextButton1.Padding = New System.Windows.Forms.Padding(0, 0, 6, 0)
+        ContextButton1.Visibility = DevExpress.Utils.ContextItemVisibility.Visible
+        ContextButton1.Width = 32
+        Me.TileView1.ContextButtons.Add(ContextButton1)
+        Me.TileView1.GridControl = Me.GridControl1
+        Me.TileView1.Name = "TileView1"
+        Me.TileView1.OptionsTiles.Orientation = System.Windows.Forms.Orientation.Vertical
+        Me.TileView1.OptionsView.ShowButtonMode = DevExpress.XtraGrid.Views.Base.ShowButtonModeEnum.ShowAlways
+        TileViewItemElement1.Column = Me.colID
+        TileViewItemElement1.Text = "colID"
+        TileViewItemElement2.Column = Me.colInfo
+        TileViewItemElement2.Text = "colInfo"
+        TileViewItemElement3.Column = Me.colCheck
+        TileViewItemElement3.Text = "colCheck"
+        TileViewItemElement4.Column = Me.colButton
+        TileViewItemElement4.Image = CType(resources.GetObject("TileViewItemElement4.Image"),System.Drawing.Image)
+        TileViewItemElement4.ImageScaleMode = DevExpress.XtraEditors.TileItemImageScaleMode.Squeeze
+        TileViewItemElement4.ImageSize = New System.Drawing.Size(32, 32)
+        TileViewItemElement4.StretchHorizontal = true
+        TileViewItemElement4.StretchVertical = true
+        TileViewItemElement4.Text = "colButton"
+        Me.TileView1.TileTemplate.Add(TileViewItemElement1)
+        Me.TileView1.TileTemplate.Add(TileViewItemElement2)
+        Me.TileView1.TileTemplate.Add(TileViewItemElement3)
+        Me.TileView1.TileTemplate.Add(TileViewItemElement4)
+        '
+        'PanelControl2
+        '
+        Me.PanelControl2.Location = New System.Drawing.Point(115, 134)
+        Me.PanelControl2.Name = "PanelControl2"
+        Me.PanelControl2.Padding = New System.Windows.Forms.Padding(2)
+        Me.PanelControl2.Size = New System.Drawing.Size(618, 67)
+        Me.PanelControl2.TabIndex = 0
+        '
+        'SimpleButton1
+        '
+        Me.SimpleButton1.Location = New System.Drawing.Point(757, 313)
+        Me.SimpleButton1.Name = "SimpleButton1"
+        Me.SimpleButton1.Size = New System.Drawing.Size(185, 60)
+        Me.SimpleButton1.TabIndex = 1
+        Me.SimpleButton1.Text = "Test"
+        '
+        'SimpleButton2
+        '
+        Me.SimpleButton2.Location = New System.Drawing.Point(757, 398)
+        Me.SimpleButton2.Name = "SimpleButton2"
+        Me.SimpleButton2.Size = New System.Drawing.Size(185, 58)
+        Me.SimpleButton2.TabIndex = 2
+        Me.SimpleButton2.Text = "SimpleButton2"
+        '
+        'SimpleButton3
+        '
+        Me.SimpleButton3.Location = New System.Drawing.Point(757, 471)
+        Me.SimpleButton3.Name = "SimpleButton3"
+        Me.SimpleButton3.Size = New System.Drawing.Size(185, 58)
+        Me.SimpleButton3.TabIndex = 3
+        Me.SimpleButton3.Text = "SimpleButton3"
+        '
+        'SimpleButton4
+        '
+        Me.SimpleButton4.Location = New System.Drawing.Point(115, 219)
+        Me.SimpleButton4.Name = "SimpleButton4"
+        Me.SimpleButton4.Size = New System.Drawing.Size(618, 67)
+        Me.SimpleButton4.TabIndex = 4
+        Me.SimpleButton4.Text = "SimpleButton4"
+        '
+        'SimpleButton5
+        '
+        Me.SimpleButton5.Dock = System.Windows.Forms.DockStyle.Top
+        Me.SimpleButton5.Location = New System.Drawing.Point(9, 9)
+        Me.SimpleButton5.Name = "SimpleButton5"
+        Me.SimpleButton5.Padding = New System.Windows.Forms.Padding(2)
+        Me.SimpleButton5.Size = New System.Drawing.Size(606, 67)
+        Me.SimpleButton5.TabIndex = 5
+        Me.SimpleButton5.Text = "SimpleButton5"
+        '
+        'SimpleButton6
+        '
+        Me.SimpleButton6.Location = New System.Drawing.Point(757, 535)
+        Me.SimpleButton6.Name = "SimpleButton6"
+        Me.SimpleButton6.Size = New System.Drawing.Size(185, 58)
+        Me.SimpleButton6.TabIndex = 5
+        Me.SimpleButton6.Text = "SimpleButton6"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(144!, 144!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi
+        Me.ClientSize = New System.Drawing.Size(1311, 977)
+        Me.Controls.Add(Me.SimpleButton6)
+        Me.Controls.Add(Me.SimpleButton4)
+        Me.Controls.Add(Me.SimpleButton3)
+        Me.Controls.Add(Me.SimpleButton2)
+        Me.Controls.Add(Me.SimpleButton1)
+        Me.Controls.Add(Me.PanelControl2)
+        Me.Controls.Add(Me.PanelControl1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.RepositoryItemButtonEdit1,System.ComponentModel.ISupportInitialize).EndInit
+        CType(Me.PanelControl1,System.ComponentModel.ISupportInitialize).EndInit
+        Me.PanelControl1.ResumeLayout(false)
+        CType(Me.GridControl1,System.ComponentModel.ISupportInitialize).EndInit
+        CType(Me.TileView1,System.ComponentModel.ISupportInitialize).EndInit
+        CType(Me.PanelControl2,System.ComponentModel.ISupportInitialize).EndInit
+        Me.ResumeLayout(false)
+
+End Sub
+
+    Friend WithEvents FormAssistant1 As DevExpress.XtraBars.FormAssistant
+    Friend WithEvents DefaultLookAndFeel1 As DevExpress.LookAndFeel.DefaultLookAndFeel
+    Friend WithEvents PanelControl1 As DevExpress.XtraEditors.PanelControl
+    Friend WithEvents PanelControl2 As DevExpress.XtraEditors.PanelControl
+    Friend WithEvents SimpleButton1 As DevExpress.XtraEditors.SimpleButton
+    Friend WithEvents GridControl1 As DevExpress.XtraGrid.GridControl
+    Friend WithEvents TileView1 As DevExpress.XtraGrid.Views.Tile.TileView
+    Friend WithEvents colID As DevExpress.XtraGrid.Columns.TileViewColumn
+    Friend WithEvents colInfo As DevExpress.XtraGrid.Columns.TileViewColumn
+    Friend WithEvents colCheck As DevExpress.XtraGrid.Columns.TileViewColumn
+    Friend WithEvents colButton As DevExpress.XtraGrid.Columns.TileViewColumn
+    Friend WithEvents RepositoryItemButtonEdit1 As DevExpress.XtraEditors.Repository.RepositoryItemButtonEdit
+    Friend WithEvents SimpleButton2 As DevExpress.XtraEditors.SimpleButton
+    Friend WithEvents SimpleButton3 As DevExpress.XtraEditors.SimpleButton
+    Friend WithEvents SimpleButton4 As DevExpress.XtraEditors.SimpleButton
+    Friend WithEvents SimpleButton5 As DevExpress.XtraEditors.SimpleButton
+    Friend WithEvents SimpleButton6 As DevExpress.XtraEditors.SimpleButton
+End Class
---
+++ frmSkinTest/Form1.vb
@@ -0,0 +1,254 @@
+﻿Imports DevExpress.LookAndFeel
+Imports DevExpress.Skins
+Imports DevExpress.Utils
+Imports DevExpress.XtraEditors
+Imports DevExpress.XtraGrid.Views.Tile
+
+Public Class Form1
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        'reSkinButton
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        GridControl1.DataSource = GetData(10)
+        'DevExpress.XtraEditors.WindowsFormsSettings.AllowDpiScale=True
+        'DevExpress.XtraEditors.WindowsFormsSettings.SetDPIAware
+        
+    End Sub
+
+    Private Sub SimpleButton1_Click(sender As Object, e As EventArgs) Handles SimpleButton1.Click
+        Dim element2 As SkinElement = SkinManager.GetSkinElement(SkinProductId.Ribbon, DevExpress.LookAndFeel.UserLookAndFeel.Default, "TabPanel")
+        'PanelControl2.LookAndFeel.UseDefaultLookAndFeel=False
+        PanelControl2.BackgroundImageLayout = ImageLayout.Stretch
+        PanelControl2.BackgroundImage = element2.Image.Image
+
+        'PanelControl2.Appearance.Options.UseImage=True
+        LookAndFeelHelper.ForceDefaultLookAndFeelChanged()
+        Debug.Print("Click")
+    End Sub
+
+    Private Sub TileView1_ContextButtonCustomize(sender As Object, e As TileViewContextButtonCustomizeEventArgs) Handles TileView1.ContextButtonCustomize
+        Dim row As DataRowView = TryCast(TileView1.GetRow(e.RowHandle), DataRowView)
+        Dim check As Boolean = CBool(row.Row("Check"))
+        e.Item.Visibility = If(check, DevExpress.Utils.ContextItemVisibility.Visible, DevExpress.Utils.ContextItemVisibility.Hidden)
+    End Sub
+
+    Private Function GetData(rows As Integer) As DataTable
+        Dim dt As New DataTable()
+        dt.Columns.Add("ID", GetType(Integer))
+        dt.Columns.Add("Info", GetType(String))
+        dt.Columns.Add("Check", GetType(Boolean))
+        For i As Integer = 0 To rows - 1
+            dt.Rows.Add(i, "Info" & i, i Mod 2 = 0)
+        Next
+        Return dt
+    End Function
+
+    Private Sub SimpleButton2_Click(sender As Object, e As EventArgs) Handles SimpleButton2.Click
+        Dim x As ContextItem = TileView1.ContextButtons(0)
+        
+        Dim y As DevExpress.XtraGrid.Columns.TileViewColumn = TileView1.Columns("colButton")
+        'As DevExpress.XtraGrid.Columns.GridColumn 
+        Dim z As TileViewItemElement= GetTileElement("colButton")
+        Debug.Print("XX")
+        y.Width=32
+        
+        
+        x.Size = New Size(12,12)
+        LookAndFeelHelper.ForceDefaultLookAndFeelChanged()
+    End Sub
+
+    Private Function GetTileElement(colName As String) As TileViewItemElement
+        For each element As TileViewItemElement In TileView1.TileTemplate
+            If element.Column.Name = colName Then Return element
+        Next
+        Return Nothing
+    End Function
+
+    Private Sub reSkinButton
+        Dim currentSkin As DevExpress.Skins.Skin
+        Dim element As DevExpress.Skins.SkinElement
+        Dim elementName As String
+        currentSkin = DevExpress.Skins.CommonSkins.GetSkin(DevExpress.LookAndFeel.UserLookAndFeel.Default)
+        elementName = DevExpress.Skins.CommonSkins.SkinButton
+
+        element = currentSkin(elementName)
+
+        Dim image As Image = element.Image.GetImages.Images(4)
+        Dim newImage = image.FromFile("d:\skin.png")
+        element.Image.SetImage(newImage,"elementName")
+        LookAndFeelHelper.ForceDefaultLookAndFeelChanged()
+    End Sub
+
+    Private Sub SimpleButton3_Click(sender As Object, e As EventArgs) Handles SimpleButton3.Click
+        Dim currentSkin As DevExpress.Skins.Skin
+        Dim element As DevExpress.Skins.SkinElement
+        Dim elementName As String
+
+        'currentSkin = DevExpress.Skins.GridSkins.GetSkin(DevExpress.LookAndFeel.UserLookAndFeel.Default)
+        'elementName = DevExpress.Skins.GridSkins.SkinHeader
+
+        currentSkin = DevExpress.Skins.CommonSkins.GetSkin(DevExpress.LookAndFeel.UserLookAndFeel.Default)
+        elementName = DevExpress.Skins.CommonSkins.SkinButton
+
+        element = currentSkin(elementName)
+
+        Dim image As Image = element.Image.GetImages.Images(4)
+        Dim newImage = image.FromFile("d:\skin.png")
+        element.Image.SetImage(newImage,"elementName")
+        'Dim img2 = New Bitmap(Image, New Size(PanelControl2.Bounds.Width, PanelControl2.Bounds.Height)) 'ctl.Height /scaleFactor.Height
+
+        Dim rr As Rectangle = PanelControl2.Bounds
+        'rr.Inflate(1,1)
+        'element.Image.GetImages(0)
+        'PanelControl2.ContentImage = Img2
+        'PanelControl2.CreateGraphics.DrawImage(image, rr, New Rectangle(3,3, image.Width-8, image.Height - 8), GraphicsUnit.Pixel)
+        'Graphics.DrawImage(bmp, rr, New Rectangle(1, 1, bmp.Width - 3, bmp.Height - 2), GraphicsUnit.Pixel)
+        'PanelControl2.BackgroundImage = image
+        LookAndFeelHelper.ForceDefaultLookAndFeelChanged()
+        
+        Dim tgtRect As Rectangle = rr
+        Dim l As Integer = tgtRect.Left
+        Dim r As Integer = tgtRect.Right
+        Dim w As Integer = tgtRect.Width
+        Dim t As Integer = tgtRect.Top
+        Dim b As Integer = tgtRect.Bottom
+        Dim h As Integer = tgtRect.Height
+
+        'Dim images As DevExpress.Utils.ImageCollection = element.Image.GetImages()
+        'bmp = images.Images(0)
+    End Sub
+
+    Private Sub PanelControl2_Paint(sender As Object, e As PaintEventArgs) Handles PanelControl2.Paint
+        Dim currentSkin As DevExpress.Skins.Skin
+        Dim element As DevExpress.Skins.SkinElement
+        Dim elementName As String
+        currentSkin = DevExpress.Skins.CommonSkins.GetSkin(DevExpress.LookAndFeel.UserLookAndFeel.Default)
+        elementName = DevExpress.Skins.CommonSkins.SkinButton
+
+        Dim image As Image
+        element = currentSkin(elementName)
+'        Dim newImage = image.FromFile("d:\skinbtn.png")
+'        element.Image.SetImage(newImage,"elementName")
+        image = element.Image.GetImages.Images(0)
+'        image = newImage
+        'element.Image.GetImage(false).Save("D:\skin.png", System.Drawing.Imaging.ImageFormat.Png)
+        Dim rr As Rectangle = PanelControl2.Bounds
+        'Dim core As Rectangle = rr.Inflate(-4,-4)
+        
+        rr.Inflate(-4,-4)
+        Dim sizingMargins = element.Image.SizingMargins
+        'e.Graphics.DrawImage(image, rr, New Rectangle(0 + sizingMargins.Left,0+ sizingMargins.Top, image.Width-sizingMargins.Width, image.Height - sizingMargins.Height), GraphicsUnit.Pixel)
+        'e.Graphics.DrawImage(image, rr, New Rectangle(0 + sizingMargins.Left,0+ sizingMargins.Top, image.Width-sizingMargins.Width, image.Height - sizingMargins.Height), GraphicsUnit.Pixel)
+        Dim image2 As Image = DrawFilledRectangle(128,128, Brushes.Yellow)
+        DrawPanel3(e.Graphics, image, sizingMargins, PanelControl2)
+    End Sub
+
+    Private Sub DrawPanel3(g As Graphics, image As Image, sM As SkinPaddingEdges, panel As PanelControl)
+        Dim tgtRect = panel.Bounds
+
+        Dim l As Integer = 0
+        Dim w As Integer = tgtRect.Width
+        Dim t As Integer = 0
+        Dim h As Integer = tgtRect.Height
+
+        Dim srcR As Rectangle = New Rectangle(0,0, image.Width, image.Height)
+        
+        '// corner squares
+        g.DrawImage(image, New Rectangle(l, t, sM.Left, sM.top), New Rectangle(0, 0, sM.Left, sM.Top), GraphicsUnit.Pixel)
+        g.DrawImage(image, New Rectangle(l, h-sM.Bottom, sM.Left, sM.Bottom), New Rectangle(0, srcR.Bottom-sM.Bottom, sM.Left, sM.Bottom), GraphicsUnit.Pixel)
+        g.DrawImage(image, New Rectangle(w-sM.Right,t,sM.Right,sM.Top), New Rectangle(srcR.Right- sM.right, 0, sM.Right, sM.Top), GraphicsUnit.Pixel)
+        g.DrawImage(image, New Rectangle(w-sM.Right, h-sM.Bottom , sM.Right, sM.Bottom), New Rectangle(srcR.Right-sM.Right, srcR.Bottom - sM.Bottom, sM.Right, sM.Bottom), GraphicsUnit.Pixel)
+
+        '// edges
+        g.DrawImage(image, New Rectangle(l+sM.Left, t, w-l-sM.Width, sM.Top), New Rectangle(sM.Left, 0, image.Width-sM.Width - 1, sM.Top), GraphicsUnit.Pixel)
+        g.DrawImage(image, New Rectangle(l+sM.Left, h-sM.Bottom, w-l-sM.Width, sM.Bottom), New Rectangle(sM.Left, srcR.Bottom-sM.Bottom, image.Width-sM.Width - 1, sM.Bottom), GraphicsUnit.Pixel)
+        g.DrawImage(image, New Rectangle(l, t+sM.Top, sM.Left, h-t-sM.Height), New Rectangle(0, sM.Top, sM.Left, image.Height - sM.Height - 1), GraphicsUnit.Pixel)
+        g.DrawImage(image, New Rectangle(w-sM.Right, t+sM.Top, sM.Right, h-t-sM.Height), New Rectangle(srcR.right - sM.Right, sM.Top, sM.Right, image.Height - sM.Height - 1), GraphicsUnit.Pixel)
+
+        '// body
+        g.DrawImage(image, New Rectangle(l+sM.Left, t+sM.Top, w-l-sM.Width, h-t-sM.Height), New Rectangle(sM.Left, sM.Top, image.Width-sM.Width-1, image.Height-sM.Height - 1), GraphicsUnit.Pixel)
+ 
+    End Sub
+
+    Private Sub DrawPanel2(g As Graphics, image As Image, sM As SkinPaddingEdges, panel As PanelControl)
+        Dim tgtRect = panel.Bounds
+        Dim l As Integer = 0
+        Dim w As Integer = tgtRect.Width
+        Dim t As Integer = 0
+        Dim h As Integer = tgtRect.Height
+
+
+        Dim srcR As Rectangle = New Rectangle(0,0, image.Width-1, image.Height-1)
+        
+        g.DrawImage(image, New Rectangle(l, t, sM.Left, sM.top), New Rectangle(0, 0, sM.Left, sM.Top), GraphicsUnit.Pixel)
+        g.DrawImage(image, New Rectangle(l, h-sM.Bottom, sM.Left, sM.Bottom), New Rectangle(0, srcR.Bottom-sM.Bottom + 1, sM.Left, sM.Bottom), GraphicsUnit.Pixel)
+        g.DrawImage(image, New Rectangle(w-sM.Right,t,sM.Right,sM.Top), New Rectangle(srcR.Right- sM.right + 1, 0, sM.Right, sM.Top), GraphicsUnit.Pixel)
+        g.DrawImage(image, New Rectangle(w-sM.Right, h-sM.Bottom , sM.Right, sM.Bottom), New Rectangle(srcR.Right-sM.Right + 1, srcR.Bottom - sM.Bottom + 1, sM.Right, sM.Bottom), GraphicsUnit.Pixel)
+
+        g.DrawImage(image, New Rectangle(l+sM.Left, t, w-l-sM.Width, sM.Top), New Rectangle(sM.Left, 0, image.Width-sM.Width - 1, sM.Top), GraphicsUnit.Pixel)
+        g.DrawImage(image, New Rectangle(l+sM.Left, h-sM.Bottom, w-l-sM.Width, sM.Bottom), New Rectangle(sM.Left, srcR.Bottom-sM.Bottom + 1, image.Width-sM.Width - 1, sM.Bottom), GraphicsUnit.Pixel)
+        g.DrawImage(image, New Rectangle(l, t+sM.Top, sM.Left, h-t-sM.Height), New Rectangle(0, sM.Top, sM.Left, image.Height - sM.Height - 1), GraphicsUnit.Pixel)
+        g.DrawImage(image, New Rectangle(w-sM.Right, t+sM.Top, sM.Right, h-t-sM.Height), New Rectangle(srcR.right - sM.Right + 1, sM.Top, sM.Right, image.Height - sM.Height - 1), GraphicsUnit.Pixel)
+
+        g.DrawImage(image, New Rectangle(l+sM.Left, t+sM.Top, w-l-sM.Width, h-t-sM.Height), New Rectangle(sM.Left, sM.Top, image.Width-sM.Width-1, image.Height-sM.Height-1), GraphicsUnit.Pixel)
+ 
+    End Sub
+
+
+
+
+    Private Sub DrawPanel(g As Graphics, image As Image, sM As SkinPaddingEdges, panel As PanelControl)
+        Dim tgtRect = panel.Bounds
+        'tgtRect.Inflate(-1,-1)
+        'Dim l As Integer = tgtRect.Left
+        'Dim r As Integer = tgtRect.Right
+        'Dim t As Integer = tgtRect.Top
+        'Dim b As Integer = tgtRect.Bottom
+        Dim l As Integer = 0
+        Dim r As Integer = (tgtRect.Width)
+        Dim t As Integer = 0
+        Dim b As Integer = tgtRect.Height
+
+
+        Dim srcR As Rectangle = New Rectangle(0,0, image.Width-1, image.Height-1)
+
+        '// top left - no scaling
+        'g.DrawImage(image, New Rectangle(l, t , l + sM.Left, t + sM.Top), New Rectangle(0, 0, sM.Left, sM.Top), GraphicsUnit.Pixel)
+        'g.DrawImage(image, New Rectangle(r-sM.Right, t , r, t + sM.Top), New Rectangle(srcR.Right - sM.Right, 0, srcR.Right, sM.Top), GraphicsUnit.Pixel)
+        'g.DrawImage(image, New Rectangle(l, b-sM.Bottom , l + sM.Left, b), New Rectangle(0, srcR.bottom-sM.Bottom, sM.Left, srcR.Bottom), GraphicsUnit.Pixel)
+        'g.DrawImage(image, New Rectangle(r-sM.Right, b-sM.Bottom , r, b), New Rectangle(srcR.Right - sM.Right, srcR.bottom-sM.Bottom, srcR.Right, srcR.Bottom), GraphicsUnit.Pixel)
+
+        Dim rr As Rectangle = New Rectangle(0,0,3,3)
+        g.DrawImage(image, New Rectangle(l, t, 4, 4), New Rectangle(0, 0, 4, 4), GraphicsUnit.Pixel)
+        g.DrawImage(image, New Rectangle(l, b-4 , 4, 4), New Rectangle(0, srcR.Bottom-4+1, 4, 4), GraphicsUnit.Pixel)
+        g.DrawImage(image, New Rectangle(r-4,t,4,4), New Rectangle(srcR.Right- 4 + 1, 0, 4, 4), GraphicsUnit.Pixel)
+        g.DrawImage(image, New Rectangle(r-4, b-4 , 4, 4), New Rectangle(srcR.Right-4+1, srcR.Bottom-4+1, 4, 4), GraphicsUnit.Pixel)
+
+        g.DrawImage(image, New Rectangle(l+4, t, r-l-8, 4), New Rectangle(4, 0, image.Width-8-1, 4), GraphicsUnit.Pixel)
+        g.DrawImage(image, New Rectangle(l+4, b-4, r-l-8, 4), New Rectangle(4, srcR.Bottom-4+1, image.Width-8-1, 4), GraphicsUnit.Pixel)
+        g.DrawImage(image, New Rectangle(l, t+4, 4, b-t-8), New Rectangle(0, 4, 4, image.Height-8-1), GraphicsUnit.Pixel)
+        g.DrawImage(image, New Rectangle(r-4, t+4, 4, b-t-8), New Rectangle(srcR.right-4+1, 4, 4, image.Height-8-1), GraphicsUnit.Pixel)
+
+        g.DrawImage(image, New Rectangle(l+4, t+4, r-l-8, b-t-8), New Rectangle(4, 4, image.Width-8-1, image.Height-8-1), GraphicsUnit.Pixel)
+ 
+    End Sub
+
+    Private Function DrawFilledRectangle(x As Integer, y As Integer, brush As Brush) As Image
+	Dim bmp As New Bitmap(x, y)
+	Using graph As Graphics = Graphics.FromImage(bmp)
+		Dim ImageSize As New Rectangle(0, 0, x, y)
+		graph.FillRectangle(brush, ImageSize)
+	End Using
+	Return bmp
+End Function
+
+    Private Sub SimpleButton6_Click(sender As Object, e As EventArgs) Handles SimpleButton6.Click
+        PanelControl2.Refresh
+    End Sub
+End Class
